381b40be06bad32266e26da0ad61a89ee482a41e
Okan Okta <oktaokan56@gmail.com>
2022-12-14 18:06:47 +0300

    toplama fonksiyonu oluşturuldu

--- topla.c
+++ topla.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+int topla(int a, int b){
+int sonuc = a+b;
+return sonuc;
+
+}
+
 int main(){
 printf("pull request \n");
-int a = 10,b=20;
 
-printf("Sonuc : %d",a+b);
+printf("Sonuc : %d",topla(10,20));
 
 return 0;
 }
+
